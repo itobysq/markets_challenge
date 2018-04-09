bf13e6a8983f1732787d4e10157b29efc7c0f19d
Isaac Sachs-Quintana <isachsquintana@tesla.com>
2018-04-09 08:12:06 -0700

    still not better than betting 0

--- timeseries_helpers.py
+++ timeseries_helpers.py
@@ -1,3 +1,4 @@
+import pandas as pd
 """
 Helper module for time series predictions
 """
@@ -6,3 +7,21 @@ def forecast_MAE():
     """
     Provides the mean average error for a model given a test data set
     """
+    return NotImplementedError
+
+def make_df_prophet_ready(my_df):
+    """
+    Strips the timezone and renames the timezone ds. The function assumes that
+    the time column is the index of the pandas dataframe
+    Args:
+        df (pandas DataFrame): dataframe with time column as the index
+    """
+    df = my_df.copy()
+    if df.empty:
+        raise Exception
+    index_col_name = df.index.name
+    df['ds'] = df.index
+    df = df.reset_index()
+    df = df.drop([index_col_name], axis=1)
+    df['ds'] = df.ds.dt.tz_localize(None)
+    return df
